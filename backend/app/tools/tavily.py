174de526acdfe859d85aaba28aa562de07a96361
sreeram0343 <sreerammurali2005@gmail.com>
2026-09-17 10:35:02 +0530

    Tavily

---
+++ backend/app/tools/tavily.py
@@ -0,0 +1,25 @@
+from tavily import TavilyClient
+
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
+tavily_client = TavilyClient(
+    api_key= os.getenv("TAVILY_API_KEY")
+)
+
+def tavily_search(query):
+    response = tavily_client.search(
+        query = query,
+        max_results = 5
+
+    )
+
+    results = []
+
+
+    
+
